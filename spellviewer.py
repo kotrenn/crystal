281e8cd8ff3517d2590ad8f6e3e0f881cca0eaac
Ramona <kotrenn@gmail.com>
2015-03-27 13:25:32 -0400

    Update spellviewer.py

--- spellviewer.py
+++ spellviewer.py
@@ -1,17 +1,19 @@
+# Class for the display of spells.
+
 from hexgridviewer import *
 from spell import *
 
 class SpellViewer(Window):
     def __init__(self, parent, spell):
         Window.__init__(self, parent)
-        self.spell = spell
-        self.grid_viewer = HexGridViewer(self.spell.grid)
+        self.spell = spell   # The spell we are currently working with
+        self.grid_viewer = HexGridViewer(self.spell.grid) # The grid viewer we are working with
 
     def display(self, dst):
-        # draw the grid
+        # Draw the grid
         self.grid_viewer.display(dst)
 
-        # draw every crystal
+        # Draw every crystal
         grid_viewer = self.grid_viewer
         grid = grid_viewer.grid
         for row in range(grid.num_rows()):
@@ -23,15 +25,18 @@ class SpellViewer(Window):
                 radius = 0.25 * grid_viewer.cell_w
                 crystal.display(dst, center, radius)
                     
-        # draw path stuff
+        # Draw path overlays to show which crystals are connected
         grid = grid_viewer.grid
         size = grid.size
         start = self.spell.get_source_locs()
         for loc in start:
             edges, cycle = self.spell.get_bfs(loc)
             color = (255, 255, 255)
+            # Valid spells do not have cycles.  Notify the player of a cycle.
             if cycle:
                 color = (255, 0, 0)
+                
+            # Draw every edge within the overlay
             for (u, v) in edges:
                 c0 = grid_viewer.get_center(u)
                 c1 = grid_viewer.get_center(v)
