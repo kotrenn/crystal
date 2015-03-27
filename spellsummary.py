599de56d00f01d50d3178596b3231a9112071c0c
Ramona <kotrenn@gmail.com>
2015-03-27 13:25:30 -0400

    Update spellsummary.py

--- spellsummary.py
+++ spellsummary.py
@@ -1,3 +1,6 @@
+# Simple interface for collecting and displaying information about a
+# spell based on the current setup within the spell.
+
 from spell import *
 from render import *
 
@@ -5,10 +8,13 @@ class SpellSummary(object):
     def __init__(self):
         return
 
+    # Render to the screen
     def display(self, dst, spell, corner):
+        # Sanity check
         if spell is None:
             return
 
+        # Simple version:  collect list of attributes and display as a string
         data = spell.get_atts()
         white = (255, 255, 255)
         draw_string(dst, data, corner, white)
